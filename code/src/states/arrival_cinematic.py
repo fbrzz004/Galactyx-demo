2b5d1a0521e8b62cc71f2a0554a95f5bcddf9bf6
pcodef <pedro200.sotarios@gmail.com>
2024-09-23 04:53:34 -0500

    Add template for all screens except Home

---
+++ code/src/states/arrival_cinematic.py
@@ -0,0 +1,13 @@
+from src.states.abstract_state import AbstractState
+
+
+class ArrivalCinematic(AbstractState):
+    def __init__(self, screen_instance):
+        AbstractState.__init__(self, screen_instance=screen_instance,
+                               background_color="Black")
+
+    def draw(self):
+        pass
+
+    def handle_events(self, event, machine_observer):
+        pass
