12360e96d879c169f113bd769290f713f17db4c7
pcodef <pedro200.sotarios@gmail.com>
2024-09-23 03:00:34 -0500

    Update the home interface to now implement the abstract class

--- code/src/states/home.py
+++ code/src/states/home.py
@@ -1,44 +1,24 @@
-from pygame.display import flip as ui_update
-from pygame.color import Color
-from pygame.event import get as get_event
-from pygame import QUIT
+from src.states.engine_state.abstract_state import AbstractState
 
 from src.ui_components.button.button_group import ButtonGroup
 
-class Home:
+class Home(AbstractState):
     def __init__(self, screen_instance):
-        # screen instance
-        self.__screen = screen_instance
-        self.__screen_rect = self.__screen.get_rect()
-
-        # appearance features
-        self.__background = Color("white")
+        AbstractState.__init__(self, screen_instance=screen_instance,
+                               background_color="White")
 
         self.__buttons = ButtonGroup(
-            screen_rect=self.__screen_rect,
+            screen_rect=self._screen_rect,
             labels=['Play', 'Options', 'Exit'],
             dimension=(100, 200),
             vertical_center=True,
             horizontal_center=True
         )
 
-        # flag for exit of ui loop
-        self.__exit = False
+    def draw(self):
+        self.__buttons.draw(self._screen)
 
-    def run(self, machine_observer):
-        # man loop for this interface
-        while not self.__exit:
-            # fill the screen with a color
-            self.__screen.fill(self.__background)
+    def handle_events(self, event):
+        self.__buttons.handle_events(event)
 
-            # draw buttons
-            self.__buttons.draw(self.__screen)
 
-            # manage the events queue
-            for event in get_event():
-                if event.type == QUIT:
-                    self.__exit = True
-                    machine_observer.exit = True
-
-            # update the interface
-            ui_update()
