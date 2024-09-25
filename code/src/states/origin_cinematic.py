f7851c6c1e28d6db94146ae2e310d2288275e86b
pcodef <pedro200.sotarios@gmail.com>
2024-09-25 02:47:09 -0500

    Add cinematic state with narrative display and continue button
    -Introduced OriginCinematic state to handle narrative sequences in the game.
    -Implemented a NarrativeDisplay to show introductory narratives with typewriter effects.
    -Added a 'Continue' button to allow users to proceed once the narrative sequence ends.
    -Set up initial background colors and layout adjustments for better visual presentation.

--- code/src/states/origin_cinematic.py
+++ code/src/states/origin_cinematic.py
@@ -1,13 +1,40 @@
+from tkinter.ttk import Treeview
+
 from src.states.abstract_state import AbstractState
+from src.ui_components.button.button import Button
+from src.ui_components.narrative_display.narrative_display import NarrativeDisplay
+from src.narratives.intro_narratives import intro_narrative
 
 class OriginCinematic(AbstractState):
     def __init__(self, screen_instance):
         AbstractState.__init__(self, screen_instance=screen_instance,
-                               background_color="Red")
+                               background_color="Black")
+
+        self.__display_narrative = NarrativeDisplay(screen_instance=screen_instance,
+                                                    narrative=intro_narrative,
+                                                    type_writer_effect=True)
+
+        self.__button_continue = Button(
+            position=(self._screen_rect.width - 20 - 100,
+                      self._screen_rect.height - 20 - 30),
+            dimension=(100, 30),
+            label='Continue',
+            backgrounds_button_color_default='White',
+            backgrounds_button_color_on_top_of='Gray',
+            label_button_color='Black'
+        )
 
     def draw(self):
-        pass
+        self.__display_narrative.run()
+
+        if self.__display_narrative.is_end():
+            self.__button_continue.draw(self._screen)
+
 
     def handle_events(self, event, machine_observer):
-        pass
+        label_button_pressed = self.__button_continue.handle_event()
+
+        if label_button_pressed == 'Continue':
+            machine_observer.ui_class = 'map_levels'
+            self._exit = True
 
