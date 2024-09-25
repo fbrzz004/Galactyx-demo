edc7e216e4bae1af7a61c41503edec18e49f1143
pcodef <pedro200.sotarios@gmail.com>
2024-09-25 05:46:15 -0500

    Add interactive button and background image to Trip state
    -Integrated a custom background image (background_image_trip.png) into the Trip state for a more immersive visual experience.
    -Added a TextButton labeled "To Jump" to the Trip state, allowing users to initiate the jump cinematic sequence interactively.
    -Configured button colors and positioning to align with the overall UI design and enhance usability.
    -Implemented event handling to trigger the jump_cinematic state upon button press, providing a seamless transition between states.

--- code/src/states/trip.py
+++ code/src/states/trip.py
@@ -1,13 +1,26 @@
 from src.states.abstract_state import AbstractState
+from src.ui_components.button.text_button import TextButton
 
 
 class Trip(AbstractState):
     def __init__(self, screen_instance):
         AbstractState.__init__(self, screen_instance=screen_instance,
-                               background_color="Black")
+                               path_image_background="..\\assets\\images\\ui\\background\\background_image_trip.png")
+
+        self.__button_to_jump = TextButton(
+            position=(self._screen_rect.width - 20 - 100,
+                      self._screen_rect.height - 20 - 30),
+            dimension=(100, 30),
+            label='To Jump',
+            backgrounds_button_color_default='White',
+            backgrounds_button_color_on_top_of='Gray',
+            label_button_color='Black'
+        )
 
     def draw(self):
-        pass
+        self.__button_to_jump.draw(self._screen)
 
     def handle_events(self, event, machine_observer):
-        pass
+        if self.__button_to_jump.handle_event():
+            machine_observer.ui_class = 'jump_cinematic'
+            self._exit = True
