112097db5cba8b2fecbb891444d721976b298fd2
pcodef <pedro200.sotarios@gmail.com>
2024-09-25 05:47:25 -0500

    Implement JumpCinematic state with custom background and navigation button
    -Added JumpCinematic state with a custom background image (background_image_jump.png) to enhance the visual storytelling experience.
    -Introduced a TextButton labeled "To Arrival" for seamless navigation from the jump cinematic to the arrival cinematic sequence.
    -Configured button position and color scheme to maintain visual consistency with other states, improving user interaction.
    -Implemented event handling to transition to the arrival_cinematic state upon button press, ensuring smooth state changes within the game flow.

--- code/src/states/jump_cinematic.py
+++ code/src/states/jump_cinematic.py
@@ -1,13 +1,25 @@
 from src.states.abstract_state import AbstractState
+from src.ui_components.button.text_button import TextButton
 
 
 class JumpCinematic(AbstractState):
     def __init__(self, screen_instance):
         AbstractState.__init__(self, screen_instance=screen_instance,
-                               background_color="Black")
+                               path_image_background="..\\assets\\images\\ui\\background\\background_image_jump.png")
+
+        self.__button_to_arrival = TextButton(
+            position=(self._screen_rect.width - 20 - 100,
+                      self._screen_rect.height - 20 - 30),
+            dimension=(100, 30),
+            label='To Arrival',
+            backgrounds_button_color_default='White',
+            backgrounds_button_color_on_top_of='Gray',
+            label_button_color='Black')
 
     def draw(self):
-        pass
+        self.__button_to_arrival.draw(self._screen)
 
     def handle_events(self, event, machine_observer):
-        pass
+        if self.__button_to_arrival.handle_event():
+            machine_observer.ui_class = 'arrival_cinematic'
+            self._exit = True
