21e86006ecf8d615ff12254df30167259e328bee
pcodef <pedro200.sotarios@gmail.com>
2024-09-25 04:55:17 -0500

    Improve text button rendering and hover state management
    -Refactored TextButton class to streamline text rendering and alignment logic.
    -Optimized label handling by caching the rendered text surface for improved performance.
    -Implemented on_top_of and on_top_of_reset methods to dynamically change the button's background color on hover.
    -Cleaned up redundant code and improved overall readability and maintainability of the button state logic.

--- code/src/ui_components/button/text_button.py
+++ code/src/ui_components/button/text_button.py
@@ -1,10 +1,10 @@
-from pygame import Rect, Color
+from pygame import Color
 from pygame.draw import rect
 from pygame.font import Font
 
-from pygame.mouse import get_pos as get_pos_mouse, get_pressed
+from src.ui_components.button.abstract_button import AbstractButton
 
-class Button:
+class TextButton(AbstractButton):
     def __init__(self,
                  position: list[float] | tuple[float, float],
                  dimension: list[float] | tuple[float, float],
@@ -13,8 +13,11 @@ class Button:
                  backgrounds_button_color_on_top_of: str,
                  label_button_color: str):
 
-        self.__rect = Rect(*position, *dimension)
-        self.__label = label
+        AbstractButton.__init__(self,
+                                position=position,
+                                dimension=dimension,
+                                label=label,
+                                scale_on_top_of=True)
 
         self.__colors = {
             'default': backgrounds_button_color_default,
@@ -25,34 +28,27 @@ class Button:
         self.__label_color = Color(label_button_color)
 
         self.__font = Font(None, 20)
+        # instance a surface with label
+        self.text_surface = self.__font.render(self._label, True, self.__label_color)
 
     def draw(self, screen_instance):
         # draw the container button
-        rect(screen_instance, self.__colors[self.__type_color_background], self.__rect)
-
-        # instance a surface with label
-        text_surface = self.__font.render(self.__label, True, self.__label_color)
+        rect(screen_instance, self.__colors[self.__type_color_background], self._rect)
 
         # get the surface centered on container
-        text_rect = text_surface.get_rect(center=self.__rect.center)
+        text_rect = self.text_surface.get_rect(center=self._rect.center)
 
         # Place the surface at the specified position using the text_rect object
-        screen_instance.blit(text_surface, text_rect)
+        screen_instance.blit(self.text_surface, text_rect)
 
-    def __on_top_of(self):
+    def _on_top_of(self):
         self.__type_color_background = 'on_top_of'
+        self._scale()
 
-    def __on_pressed(self):
+    def _on_top_of_reset(self):
+        self.__type_color_background = 'default'
+        self._un_scale()
+
+    def _on_pressed(self):
         pass
 
-    def handle_event(self):
-        if self.__rect.collidepoint(*get_pos_mouse()):
-            self.__on_top_of()
-
-            if get_pressed()[0]:
-                self.__on_pressed()
-                return self.__label
-        else:
-            self.__type_color_background = 'default'
-
-        return None
