b33a78cf46761d3028b0df4f9b3fad41701a7d50
pcodef <pedro200.sotarios@gmail.com>
2024-09-25 04:53:31 -0500

    Implement dynamic scaling for ImageButton with hover effect
    -added imagebutton class inheriting from abstractbutton to support dynamic image scaling on hover.
    -implemented logic to maintain original and scaled images for smooth transitions between states.
    -added _on_top_of and _on_top_of_reset methods to manage the hover effect, switching between scaled and unscaled images.
    -integrated draw method to render the appropriate image state on the screen.

---
+++ code/src/ui_components/button/image_button.py
@@ -0,0 +1,49 @@
+from pygame.transform import scale
+from src.ui_components.button.abstract_button import AbstractButton
+from pygame.image import load as load_image
+
+class ImageButton(AbstractButton):
+    def __init__(self,
+                 position: list[float] | tuple[float, float],
+                 dimension: list[float] | tuple[float, float],
+                 label: str,
+                 path_image: str):
+
+        AbstractButton.__init__(self,
+                                position=position,
+                                dimension=dimension,
+                                label=label,
+                                scale_on_top_of=True)
+
+        # original and scaled image
+        self.__image = self.__original_image = scale(load_image(path_image).convert_alpha(), dimension)
+        self.__scaled_image = None
+
+        # flag to manage the one pressed on unpressed actions
+        self.__scaled = True
+        self.__un_scaled = True
+
+    def _on_pressed(self):
+        print(self._label)
+
+    def _on_top_of(self):
+        if self.__scaled:
+            self.__un_scaled = True
+            self.__scaled = False
+            self._scale()
+
+            if not self.__scaled_image:
+                self.__scaled_image = scale(self.__original_image,
+                                            (self._rect.width, self._rect.height)).convert_alpha()
+
+            self.__image = self.__scaled_image
+
+    def _on_top_of_reset(self):
+        if self.__un_scaled:
+            self.__scaled = True
+            self.__un_scaled = False
+            self._un_scale()
+            self.__image = self.__original_image
+
+    def draw(self, screen_instance):
+        screen_instance.blit(self.__image, self._rect)
