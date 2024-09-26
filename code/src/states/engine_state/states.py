ed21bae5d25047ee4a0dfe049a9ee6fef47a6a5b
pcodef <pedro200.sotarios@gmail.com>
2024-09-26 03:51:18 -0500

    Change a structure of states on states.py

--- code/src/states/engine_state/states.py
+++ code/src/states/engine_state/states.py
@@ -1,8 +1,7 @@
 from src.states.home import Home
 from src.states.origin_cinematic import OriginCinematic
 from src.states.map_levels import MapLevels
-from src.states.trip import Trip
-from src.states.jump_cinematic import JumpCinematic
+from src.states.trip.trip import Trip
 from src.states.arrival_cinematic import ArrivalCinematic
 from src.states.end import End
 
@@ -11,7 +10,6 @@ navigation = {
     'origin_cinematic': OriginCinematic,
     'map_levels': MapLevels,
     'trip': Trip,
-    'jump_cinematic': JumpCinematic,
     'arrival_cinematic': ArrivalCinematic,
     'end': End
 }
