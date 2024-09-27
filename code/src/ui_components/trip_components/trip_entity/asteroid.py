3ac6937a064a673d9e54b1e19bdc25e9bc9358ac
pcodef <pedro200.sotarios@gmail.com>
2024-09-27 05:21:39 -0500

    Add Asteroid class with random properties for dynamic asteroid generation
    - Implement Asteroid class to handle creation and movement of asteroids.
    - Randomize asteroid size, rotation, initial position, and velocity.
    - Added method for scaling the asteroid image with random size factor.
    - Added method for rotating the asteroid image with random angle.
    - Implemented initial position outside of the screen with random horizontal positioning.
    - Randomized initial velocity in both x and y directions.
    - Included screen boundary check to reset the asteroid properties when it moves off-screen.

---
+++ code/src/ui_components/trip_components/trip_entity/asteroid.py
@@ -0,0 +1,62 @@
+from random import uniform, randint
+
+from pygame.image import load as load_image
+from pygame.transform import scale, rotate
+
+
+class Asteroid:
+    def __init__(self, screen_instance):
+        self.__screen = screen_instance
+        self.__screen_rect = screen_instance.get_rect()
+
+        # initial config image
+        self.__standard_size_factor = 1/8
+        self.__image = load_image("..\\assets\\images\\asteroid\\asteroid.png")
+        self.__image = scale(self.__image, (self.__image.get_rect().width * self.__standard_size_factor,
+                                            self.__image.get_rect().height * self.__standard_size_factor))
+        self.__original_image = self.__image.copy()
+
+
+        self.__random_scale_image()
+        self.__random_rotate_image()
+        self.__random_initial_position()
+        self.__random_initial_velocity()
+
+    def __random_scale_image(self):
+        # random size config
+        randon_size_factor = uniform(0.5, 1.5)
+        self.__image_rect = self.__original_image.get_rect()
+        self.__image_rect.width *= randon_size_factor
+        self.__image_rect.height *= randon_size_factor
+
+        self.__image = scale(self.__original_image, (self.__image_rect.width, self.__image_rect.height))
+
+    def __random_rotate_image(self):
+        # random rotate config
+        self.__image = rotate(self.__image, uniform(0, 360))
+        self.__image_rect = self.__image.get_rect()
+
+    def __random_initial_position(self):
+        self.__image_rect.x = randint(0, self.__screen_rect.width - self.__image_rect.width)
+        self.__image_rect.y = -150
+
+    def __random_initial_velocity(self):
+        self.__velocity_x = uniform(-1, 1)
+        self.__velocity_y = uniform(1, 4)
+
+    def __moving(self):
+        if self.__image_rect.y > self.__screen_rect.height + 5:
+            self.__random_scale_image()
+            self.__random_rotate_image()
+            self.__random_initial_position()
+            self.__random_initial_velocity()
+
+        else:
+            self.__image_rect.y += self.__velocity_y
+            self.__image_rect.x += self.__velocity_x
+
+    def run(self):
+        self.__moving()
+
+        # draw on the screen
+        self.__screen.blit(self.__image, self.__image_rect)
