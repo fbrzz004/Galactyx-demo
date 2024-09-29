4e2635e30ef086a3b00145eb6244b783919095cc
pcodef <pedro200.sotarios@gmail.com>
2024-09-29 17:19:54 -0500

    Refactor Spacecraft and Jet classes to improve jet handling and event-driven control
    - Implement `directional_jet` feature in `Spacecraft` to control left and right jet behavior during movement.
    - Modify the `Jet` class and its initialization for more modular handling of jet positions and velocities.
    - Adjust event handling in `Spacecraft` to interact with the newly added `directional_jet` methods for directional flame control.
    - Refactor the `MainJet` class to improve the positioning and movement of individual flame units relative to the spacecraft.
    - Add new methods to control left (`off_left`) and right (`off_right`) directional jets based on keyboard input.
    - Clean up redundant code and enhance jet repositioning logic for smoother transitions.

--- code/src/ui_components/trip_components/trip_entity/spacecraft/jet.py
+++ code/src/ui_components/trip_components/trip_entity/spacecraft/jet.py
@@ -7,7 +7,9 @@ from pygame.draw import circle
 class Jet(ABC):
     def __init__(self, screen_instance,
                  x_positions,
-                 image_rect, velocity_x=0,
+                 y_init,
+                 image_rect, velocity_x: list,
+                 jet_active:list,
                  velocity_y=0,
                  amount=10):
 
@@ -21,8 +23,12 @@ class Jet(ABC):
         self._amount = amount
 
         # set the statics x, y initial position
-        self._y_init = self._image_rect.y + self._image_rect.height
-        self._x_jet = list(x_positions(self._image_rect))
+        self._y_init = y_init
+        self._x_jet_lambda_current = x_positions
+        self._x_jet_lambda_past = self._x_jet_lambda_current(self._image_rect)
+
+        # jet state
+        self._jet_active = jet_active
 
         # set the list with all list (x, y, radio) that represent
         # each circle of jet's flame
@@ -39,34 +45,40 @@ class Jet(ABC):
 
     def __init_list_unit(self):
         self._list_unit_jet = []
-        for x_init in self._x_jet:
-            for _ in range(int(self._amount / len(self._x_jet))):
-                self._list_unit_jet.append([x_init, self._y_init, self._initial_radio])
-
-    def _init_post_unit(self):
-        for x_index, x_init in enumerate(self._x_jet):
-            for unit_jet in self._list_unit_jet[int(x_index * len(self._x_jet) / len(self._x_jet)):
-                                                 int((x_index + 1) * len(self._x_jet) / len(self._x_jet))]:
-                unit_jet[0] = self._x_jet[x_index]
-                unit_jet[1] = self._y_init
-                unit_jet[2] = self._initial_radio
-
-    @abstractmethod
-    def _move(self):
-        pass
+        for x_init in self._x_jet_lambda_current(self._image_rect):
+            line_flame = []
+            for _ in range(int(self._amount / len(self._x_jet_lambda_current(self._image_rect)))):
+                line_flame.append([x_init, self._y_init, self._initial_radio])
+            self._list_unit_jet.append(line_flame)
 
     def _draw(self):
-        for unit_jet in self._list_unit_jet:
-            circle(self.__screen, self.__color(),
-                   (uniform(unit_jet[0] - 10,unit_jet[0] + 10), unit_jet[1]),
-                   uniform(unit_jet[2] - 4, unit_jet[2]))
+        for index, vx in enumerate(self._velocity_x):
+            if self._jet_active[index]:
+                for unit_jet in self._list_unit_jet[index]:
+                    circle(self.__screen, self.__color(),
+                           (uniform(unit_jet[0] - 10,unit_jet[0] + 10), unit_jet[1]),
+                           uniform(unit_jet[2] - 4, unit_jet[2]))
+
+    def _move(self):
+        for index, vx in enumerate(self._velocity_x):
+            if self._jet_active[index]:
+                for unit_jet in self._list_unit_jet[index]:
+                    unit_jet[0] += int(self._x_jet_lambda_current(self._image_rect)[index] - self._x_jet_lambda_past[index])
+                    unit_jet[0] += uniform(vx - 2, vx + 2)
+                    unit_jet[1] += uniform(self._velocity_y - 4, self._velocity_y + 4)
+                self._x_jet_lambda_past[index] = self._x_jet_lambda_current(self._image_rect)[index]
+
+    def _check_x_position(self, y_stop):
+        for x_index, x_init in enumerate(self._x_jet_lambda_current(self._image_rect)):
+            if self._jet_active[x_index]:
+                for unit_jet in self._list_unit_jet[x_index]:
+                    if unit_jet[1] > y_stop():
+                        unit_jet[0] = x_init
+                        unit_jet[1] = self._y_init
+                        unit_jet[2] = self._initial_radio
 
     @abstractmethod
     def run(self):
-        pass
-
-    @abstractmethod
-    def _check_x_position(self):
         pass
 
 
@@ -80,28 +92,56 @@ class MainJet(Jet):
             imager.x + imager.width / 2 + 15
         ]
 
+        self.__y_init = image_rect.y + image_rect.height
+
         Jet.__init__(self, screen_instance=screen_instance,
                      x_positions=self._gen_three_x_pos,
-                     image_rect=image_rect, velocity_y=4, amount=amount)
+                     y_init=self.__y_init,
+                     image_rect=image_rect, velocity_y=4, velocity_x=[0, 1, -1], amount=amount,
+                     jet_active=[1, 1, 1])
 
-        self.__y_stop = self._y_init + 100
-
-    def _check_x_position(self):
-        for jet_unit in self._list_unit_jet:
-            if jet_unit[1] > uniform(self.__y_stop - 140, self.__y_stop + 240):
-                jet_unit[0] = self._gen_three_x_pos(self._image_rect)[randint(0, 2)]
-                jet_unit[1] = self._y_init
-                jet_unit[2] = self._initial_radio
-
-    def _move(self):
-        for unit_jet in self._list_unit_jet:
-            unit_jet[0] = self._gen_three_x_pos(self._image_rect)[randint(0, 2)]
-            unit_jet[1] += uniform(self._velocity_y - 10, self._velocity_y + 10)
+        self.__y_stop = lambda : uniform(self._y_init + 100 - 140, self._y_init + 100 + 240)
 
     def run(self):
         self._move()
-        self._check_x_position()
+        self._check_x_position(self.__y_stop)
         self._draw()
+
+class DirectionalJet(Jet):
+    def __init__(self, screen_instance, image_rect, amount=10):
+
+        self.__gen_three_x_pos = lambda imager : [imager.x + imager.width / 2 - 60,
+                                                  imager.x + imager.width / 2 + 60]
+
+        self.__y_init = image_rect.y + image_rect.height - 35
+
+        Jet.__init__(self, screen_instance=screen_instance,
+                     x_positions=self.__gen_three_x_pos,
+                     y_init=self.__y_init,
+                     image_rect=image_rect,
+                     velocity_y=4, velocity_x=[-5, 5], amount=amount,
+                     jet_active=[0, 0])
+
+        self.__y_stop = lambda : uniform(self._y_init + 60 - 140, self._y_init + 60 + 240)
+
+    def run(self):
+        self._move()
+        self._check_x_position(self.__y_stop)
+        self._draw()
+
+    def on_left(self):
+        self._jet_active[0] = 1
+
+    def on_right(self):
+        self._jet_active[1] = 1
+
+    def off_left(self):
+        self._jet_active[0] = 0
+
+    def off_right(self):
+        self._jet_active[1] = 0
+
+
 
 
 
