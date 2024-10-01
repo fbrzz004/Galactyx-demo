7cd783762a867cfc719bf4a68a8a27fef290db52
pcodef <pedro200.sotarios@gmail.com>
2024-10-01 07:17:50 -0500

    Add energy management and auto-recovery logic to Weapon class
    - Implemented `max_energy` attribute to define the maximum energy limit for the weapon (default: 200).
    - Added `auto_get_energy()` method to automatically regenerate energy when below maximum and when conditions are met.
    - Introduced `get_energy()` method to return the current energy level and its relation to the maximum energy.
    - Enhanced `shoot()` method to correctly subtract energy with each shot and ensure energy doesn't go negative.
    - Added live status management methods (`get_live()` and `is_live()`) to track and validate the spacecraft's life status during gameplay.
    - Integrated gradual life regeneration when energy conditions are satisfied.

--- code/src/ui_components/trip_components/trip_entity/spacecraft/weapon.py
+++ code/src/ui_components/trip_components/trip_entity/spacecraft/weapon.py
@@ -4,16 +4,20 @@ from .bullet.bullet import Bullet
 
 class Weapon:
     def __init__(self, screen_instance,
-                 spacecraft_rect):
+                 spacecraft_rect,
+                 get_energy):
         self.__screen = screen_instance
         self.__spacecraft_rect = spacecraft_rect
 
-        self.__amount_energy = 100
+        self.__amount_energy = 200
+        self.__max_energy = 200
 
         self.__x_muzzle = lambda spa_r : [
             spa_r.x + spa_r.width / 2 - 25,
             spa_r.x + spa_r.width / 2 + 20,
         ]
+
+        self.__get_energy=get_energy
 
     def shoot(self, manager_bullet: ManagerBullet, energy):
         if self.__amount_energy > 0:
@@ -24,5 +28,12 @@ class Weapon:
                        energy=energy)
             )
             self.__amount_energy -= energy
+
+    def auto_get_energy(self):
+        if self.__amount_energy < self.__max_energy and self.__get_energy(self.__spacecraft_rect):
+            self.__amount_energy += 1
+
+    def get_energy(self):
+        return self.__amount_energy, self.__max_energy
 
 
