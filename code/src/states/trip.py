c695da79d20ebbdbe63347cf3ed64d9b947f132e
pcodef <pedro200.sotarios@gmail.com>
2024-10-01 07:21:17 -0500

    Refactor Trip class to manage game state transitions and button interactions
    - Refactored the `Trip` class to manage sub-state transitions, including `GameTrip`, `JumpTrip`, and `GameOverTrip`.
    - Integrated `TextButton` for handling user input to jump between different game states.
    - Updated `draw()` method to render both the main game interface and the button for state transitions.
    - Enhanced the `handle_events()` method to handle user input, including button clicks that lead to state changes.
    - Improved state transitions by removing and reinitializing sub-states like `GameTrip` and `JumpTrip` based on user input (e.g., exit or retry).

--- code/src/states/trip.py
+++ code/src/states/trip.py
@@ -1,10 +1,14 @@
 from src.states.abstract_state import AbstractState
+from src.states.trip_sub_state.game_over_trip import GameOverTrip
 from src.states.trip_sub_state.game_trip import GameTrip
+from src.states.trip_sub_state.jump_cinematic import JumpTrip
 from src.ui_components.button.text_button import TextButton
 
 class Trip(AbstractState):
     def __init__(self, screen_instance):
         AbstractState.__init__(self, screen_instance=screen_instance)
+        
+        self.__screen = screen_instance
 
         self.__button_to_jump = TextButton(
             position=(self._screen_rect.width - 20 - 150,
@@ -17,19 +21,37 @@ class Trip(AbstractState):
         )
 
         # test the jump interface
-        self.__game_trip = GameTrip(screen_instance)
+        self.__game_trip_sub_state = GameTrip(screen_instance)
 
     def draw(self):
-
-        # test the jump interface
-        self.__game_trip.draw()
-
-
+        self.__game_trip_sub_state.draw()
         self.__button_to_jump.draw(self._screen)
-
 
 
     def handle_events(self, event, machine_observer):
         if self.__button_to_jump.handle_event():
             machine_observer.exit = True
             self._exit = True
+
+        state = self.__game_trip_sub_state.handle_events(event)
+        
+        if state == 'Exit':
+            machine_observer.exit = True
+            self._exit = True
+            
+        if state == 'Again':
+            del self.__game_trip_sub_state
+            self.__game_trip_sub_state = GameTrip(self.__screen)
+            
+        if state == 'Jump':
+            del self.__game_trip_sub_state
+            self.__game_trip_sub_state = JumpTrip(self.__screen)
+
+        if state == 'game_over':
+            del self.__game_trip_sub_state
+            self.__game_trip_sub_state = GameOverTrip(self.__screen)
+
+        if state == 'Home':
+            self._exit = True
+            machine_observer.ui_class = 'home'
+            
