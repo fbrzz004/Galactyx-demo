4d5dac1469d6673f3dc48b83d222075cb23888e8
pcodef <pedro200.sotarios@gmail.com>
2024-10-01 07:22:02 -0500

    Enhance GameTrip class with collision management, weapon system, and energy HUD
    - Added `ManagerBullet` instance to handle projectile management for the spacecraft's weapon.
    - Integrated `SpacecraftCollisionManager` to manage collisions for the spacecraft, including checking asteroid collisions.
    - Introduced `EnergyHud` to visualize the current energy status of the spacecraft, displaying it on the screen.
    - Updated `draw()` method to render the energy HUD alongside the background, asteroids, and bullets.
    - Enhanced event handling by verifying collisions between the spacecraft and asteroids, updating the game state based on the collision outcomes.
    - Improved overall game state handling by managing player life and energy through the `SpacecraftCollisionManager` and `EnergyHud`.

--- code/src/states/trip_sub_state/game_trip.py
+++ code/src/states/trip_sub_state/game_trip.py
@@ -5,6 +5,9 @@ from src.ui_components.trip_components.trip_entity.dyson_sphere import DysonSphe
 from src.ui_components.trip_components.trip_entity.spacecraft.spacecraft import Spacecraft
 
 from ...ui_components.trip_components.trip_entity.spacecraft.bullet.manager_bullet import ManagerBullet
+from ...ui_components.trip_components.trip_entity.spacecraft.spacecraft_collision_manager import \
+    SpacecraftCollisionManager
+from ...ui_components.trip_components.trip_hud.energy_hud import EnergyHud
 
 
 class GameTrip:
@@ -29,14 +32,28 @@ class GameTrip:
         )
 
         self.__player_spacecraft = Spacecraft(
-            screen_instance=screen_instance
+            screen_instance=screen_instance,
+            get_energy = self.__dyson_sphere.into_energy
         )
 
         # this obj manage the bullet spacecraft's weapon
         self.__manager_bullet = ManagerBullet()
 
+        # this obj manage the spacecraft collision
+        self.__manager_spacecraft_collision = SpacecraftCollisionManager(
+            spacecraft_rect=self.__player_spacecraft.get_rect(),
+            get_energy=self.__dyson_sphere.into_energy
+        )
+
+        # hud implementation+
+        self.__energy_weapon_hud = EnergyHud(screen_instance=screen_instance,
+                                         get_current_level=self.__player_spacecraft.get_energy_weapon)
+
     def __verify_collision_bullet_with_other_objet(self):
         self.__manager_bullet.is_collision(self.__group_asteroid.get_rect_callable_collision())
+
+    def __verify_collision_spacecraft_to_asteroids(self):
+        self.__manager_spacecraft_collision.is_collision(self.__group_asteroid.get_rect_callable_collision())
 
     def draw(self):
         # draw background star
@@ -57,7 +74,20 @@ class GameTrip:
         # run the manager of bullet
         self.__manager_bullet.run()
 
+        # hud section
+        # hud energy weapon
+        self.__energy_weapon_hud.run()
+
     def handle_events(self, event):
         self.__player_spacecraft.handler(event, manager_bullet=self.__manager_bullet)
         self.__verify_collision_bullet_with_other_objet()
+        self.__verify_collision_spacecraft_to_asteroids()
+
+        state = None
+
+        if not self.__manager_spacecraft_collision.is_live():
+            state = 'game_over'
+
+        return state
+
 
