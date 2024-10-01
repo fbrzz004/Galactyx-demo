bb3196e85fcb698ebba6c66b3e73b2279c3ebca3
pcodef <pedro200.sotarios@gmail.com>
2024-10-01 04:11:19 -0500

    Refactor ManagerBullet collision detection and improve Weapon shooting mechanism
    - Updated `ManagerBullet` class to enhance bullet collision detection with object rects:
      - Added logic to copy and remove bullet rects after detecting collisions.
      - Improved collision handling for both single and multiple objects using `singledispatch`.
      - Refactored the logic for removing bullets that are off-screen.
    
    - Improved `Weapon` class by refining the `shoot()` method:
      - Now shoots bullets from specified muzzle positions relative to the spacecraft.
      - Added energy management to ensure bullets are only fired if there is enough energy.
      - Decreased energy level after each shot based on the `energy` parameter provided.

--- code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/manager_bullet.py
+++ code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/manager_bullet.py
@@ -10,25 +10,27 @@ class ManagerBullet:
         self.__bullets.append(bullet)
 
     @singledispatch
-    def is_collision(self, rect_of_collision: Rect, set_collision_rect: callable):
+    def is_collision(self, rect_of_collision, set_collision_rect):
         for bullet in self.__bullets:
-            for rect_b in bullet.rects:
+            for rect_b in bullet.get_rects().copy():
                 if rect_b.colliderect(rect_of_collision):
-                    bullet.rects.remove(rect_b)
+                    bullet.get_rects().remove(rect_b)
                     set_collision_rect()
+                    break
                 if rect_b.y + rect_b.height < 0:
-                    bullet.rects.remove(rect_b)
+                    bullet.get_rects().remove(rect_b)
 
-    @is_collision.register
-    def _(self, rect_of_collisions: list):
-        for bullet in self.__bullets:
-            for rect_b in bullet.rects:
-                for rect_of_collision, set_collision_rect in rect_of_collisions:
+    @is_collision.register(list)
+    def is_collision(self, list_rect_of_collisions):
+        for rect_of_collision, set_collision_rect in list_rect_of_collisions:
+            for bullet in self.__bullets:
+                for rect_b in bullet.get_rects().copy():
                     if rect_b.colliderect(rect_of_collision):
-                        bullet.rects.remove(rect_b)
+                        bullet.get_rects().remove(rect_b)
                         set_collision_rect()
+                        break
                     if rect_b.y + rect_b.height < 0:
-                        bullet.rects.remove(rect_b)
+                        bullet.get_rects().remove(rect_b)
 
     def run(self):
         for bullet in self.__bullets:
