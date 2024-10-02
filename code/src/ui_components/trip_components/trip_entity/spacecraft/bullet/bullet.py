7fddb0025c9a67fcc8f9b90ce40a71e33200f1c2
pcodef <pedro200.sotarios@gmail.com>
2024-10-02 08:44:55 -0500

    Enhance Bullet class with dynamic velocity and length calculation based on screen height
    - Added `velocity_y` parameter to allow for dynamic bullet speed adjustments.
    - Updated bullet length calculation formula to scale based on the screen's height, ensuring consistent behavior across different screen resolutions.
    - Refined the `__auto_move()` method to use the dynamic velocity and ensure bullets are removed when they go off-screen.
    - Improved bullet initialization to accommodate different energy levels and velocities.

--- code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/bullet.py
+++ code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/bullet.py
@@ -5,28 +5,26 @@ from math import pow
 class Bullet:
     def __init__(self, screen_instance,
                  x_muzzle: list, y_muzzle: int,
-                 energy: int):
+                 velocity_y=-20):
         self.__screen = screen_instance
         self.__screen_rect = screen_instance.get_rect()
 
         self.__min_length = 5
         self.__max_length = 200
 
-        self.__length = lambda y: ((-4 * (self.__max_length - self.__min_length) / pow(y_muzzle, 2)) * pow(y, 2) +
-                                   (4 * (self.__max_length - self.__min_length) / y_muzzle) * y +
+        self.__length = lambda y: ((-4 * (self.__max_length - self.__min_length) / pow(self.__screen_rect.height, 2)) * pow(y, 2) +
+                                   (4 * (self.__max_length - self.__min_length) / self.__screen_rect.height) * y +
                                    5)
 
-        self.__velocity_y = 20
+        self.__velocity_y = velocity_y
 
         self.__rects = [Rect(x, y_muzzle, 4, self.__min_length) for x in x_muzzle]
 
-        self.__energy = energy
-
     def __auto_move(self):
         for r in self.__rects:
-            r.y -= self.__velocity_y
+            r.y += self.__velocity_y
             r.height = self.__length(r.y)
-            if r.y < -self.__max_length:
+            if not (0 < r.y < self.__screen_rect.height):
                 self.__rects.remove(r)
 
     def __draw(self):
