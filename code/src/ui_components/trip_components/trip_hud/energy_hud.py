30570eb37b78d83b1d29550c5535614534fd30ba
pcodef <pedro200.sotarios@gmail.com>
2024-10-01 20:04:35 -0500

    Enhance EnergyHud with title display using NameText
    - Integrated `NameText` class into `EnergyHud` to display a customizable title above the energy bar.
    - Updated `__init__()` method to accept parameters for positioning (`left`, `bottom`, `right`) and a title string.
    - Adjusted positioning logic for the energy bar and title to allow for dynamic alignment based on the provided coordinates.
    - Refactored rectangle setup to ensure consistent rendering of the energy bar outline and fill.
    - Improved the `draw()` method to render both the energy bar and its title on the screen.

--- code/src/ui_components/trip_components/trip_hud/energy_hud.py
+++ code/src/ui_components/trip_components/trip_hud/energy_hud.py
@@ -1,9 +1,11 @@
 from pygame import Rect
-from pygame.draw import rect 
+from pygame.draw import rect
+
+from src.ui_components.name_text.name_text import NameText
 
 
 class EnergyHud:
-    def __init__(self, screen_instance, get_current_level):
+    def __init__(self, screen_instance, get_current_level, left=None, bottom=None, right=None, title="Default"):
         self.__screen = screen_instance
         self.__screen_rect = screen_instance.get_rect()
 
@@ -11,10 +13,18 @@ class EnergyHud:
         self.__outline_color = 'white'
 
         self.__max_width_rect = 400
-        self.__height = 60
+        self.__height = 30
 
-        self.__rect_outline = Rect(10, self.__screen_rect.height - self.__height - 10, self.__max_width_rect, self.__height)
+        self.__rect_outline = Rect(left if left else (right - self.__max_width_rect), bottom - self.__height, self.__max_width_rect, self.__height)
         self.__rect_energy = self.__rect_outline.copy()
+
+        # title
+        self.__title = NameText(
+            screen_instance=screen_instance,
+            size=22,
+            text=title,
+            on_top_of_rect=self.__rect_outline
+        )
 
     def __set_color_energy(self):
         level = self.__rect_energy.width
@@ -34,6 +44,9 @@ class EnergyHud:
         self.__rect_energy.width = int(self.__max_width_rect * factor) if int(self.__max_width_rect * factor) > 0 else 0
 
     def run(self):
+
+        self.__title.show()
+
         self.__update_the_rect_energy()
         if self.__rect_energy.width != self.__max_width_rect:
             rect(self.__screen, self.__outline_color, self.__rect_outline, width=1, border_radius=5)
