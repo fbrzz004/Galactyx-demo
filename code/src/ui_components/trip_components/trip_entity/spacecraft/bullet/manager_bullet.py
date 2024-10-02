264c9cc544e58d352254dfbd707e1a090ecc7e36
pcodef <pedro200.sotarios@gmail.com>
2024-10-02 08:44:04 -0500

    Enhance ManagerBullet with bullet cleanup and collision management
    - Added `clear_bullets()` method to remove bullets that are off-screen, ensuring efficient memory management and performance.
    - Refined the `is_collision()` method to handle collisions between bullets and other objects, removing bullets upon collision or when they exit the screen.
    - Optimized collision handling with `singledispatch` to manage different types of collisions.
    - Improved `run()` method to continuously update bullet states and handle their behavior during gameplay.

--- code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/manager_bullet.py
+++ code/src/ui_components/trip_components/trip_entity/spacecraft/bullet/manager_bullet.py
@@ -1,26 +1,24 @@
-from functools import singledispatch
 from .bullet import Bullet
-from pygame import Rect
 
 class ManagerBullet:
-    def __init__(self):
+    def __init__(self, screen_width):
         self.__bullets = []
+        self.__screen_width = screen_width
+        self.__f = 0
 
     def add_bullet(self, bullet: Bullet):
         self.__bullets.append(bullet)
 
-    @singledispatch
-    def is_collision(self, rect_of_collision, set_collision_rect):
+    def get_bullets(self):
+        return self.__bullets
+
+    def is_collision_only_one(self, rect_of_collision, set_collision_rect):
         for bullet in self.__bullets:
             for rect_b in bullet.get_rects().copy():
                 if rect_b.colliderect(rect_of_collision):
                     bullet.get_rects().remove(rect_b)
                     set_collision_rect()
-                    break
-                if rect_b.y + rect_b.height < 0:
-                    bullet.get_rects().remove(rect_b)
 
-    @is_collision.register(list)
     def is_collision(self, list_rect_of_collisions):
         for rect_of_collision, set_collision_rect in list_rect_of_collisions:
             for bullet in self.__bullets:
@@ -28,10 +26,21 @@ class ManagerBullet:
                     if rect_b.colliderect(rect_of_collision):
                         bullet.get_rects().remove(rect_b)
                         set_collision_rect()
+
+
+    def __clear_bullets(self):
+        for bullet in self.__bullets.copy():
+            if bullet.get_rects():
+                for rect_b in bullet.get_rects():
+                    if not (0 < rect_b.y + rect_b.height < self.__screen_width):
+                        self.__bullets.remove(bullet)
                         break
-                    if rect_b.y + rect_b.height < 0:
-                        bullet.get_rects().remove(rect_b)
+            else:
+                self.__bullets.remove(bullet)
+
 
     def run(self):
         for bullet in self.__bullets:
             bullet.run()
+
+        self.__clear_bullets()
