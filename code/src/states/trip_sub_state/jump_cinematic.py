2f5d4de1063f283b2c6f6f72f6c60978d554cec6
pcodef <pedro200.sotarios@gmail.com>
2024-10-02 15:40:55 -0500

    Add spacecraft to JumpTrip cinematic and implement countdown logic
    - Integrated an uncontrolled `Spacecraft` in the `JumpTrip` cinematic state.
    - Implemented countdown logic with `_count_return` to manage state transitions.
    - Added functionality to dynamically update and run the spacecraft.

--- code/src/states/trip_sub_state/jump_cinematic.py
+++ code/src/states/trip_sub_state/jump_cinematic.py
@@ -1,4 +1,5 @@
 from src.ui_components.trip_components.trip_background.wormhole_background import WormholeBackground
+from src.ui_components.trip_components.trip_entity.spacecraft.spacecraft import Spacecraft
 
 
 class JumpTrip:
@@ -8,5 +9,23 @@ class JumpTrip:
             screen_instance=screen_instance
         )
 
+        self.__spacecraft_no_controlled = Spacecraft(
+            screen_instance=screen_instance,
+            static=True
+        )
+
+        self.__count_return = 100
+
     def draw(self):
         self.__wormhole_background.draw()
+        self.__spacecraft_no_controlled.run()
+
+    def return_next_sub_state(self):
+        if self.__count_return <= 0:
+            return 'Again'
+        else:
+            self.__count_return -= 1
+        return None
+
+    def handle_events(self, event):
+        pass
