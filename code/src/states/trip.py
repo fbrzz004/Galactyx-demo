18435b19840db6bcc9f894a7347e11ad80f6bed7
pcodef <pedro200.sotarios@gmail.com>
2024-10-02 21:49:53 -0500

    Integrate MapHud into Trip class and manage jump progression
    - Added `MapHud` integration to the `Trip` class for visualizing jump progression along a timeline.
    - Implemented logic to activate and deactivate the `MapHud` based on game states (jump, arrival, game over).
    - Managed jump progression through `set_jump` and `is_end` methods in `MapHud`.
    - Adjusted game state transitions to work with `MapHud`, including the deactivation when the game ends.

--- code/src/states/trip.py
+++ code/src/states/trip.py
@@ -2,19 +2,29 @@ from src.states.abstract_state import AbstractState
 from src.states.trip_sub_state.game_over_trip import GameOverTrip
 from src.states.trip_sub_state.game_trip import GameTrip
 from src.states.trip_sub_state.jump_cinematic import JumpTrip
+from src.ui_components.trip_components.trip_hud.map_hud import MapHud
+
 
 class Trip(AbstractState):
     def __init__(self, screen_instance):
         AbstractState.__init__(self, screen_instance=screen_instance)
-        
+
         self.__screen = screen_instance
 
-        # test the jump interface
-        self.__game_trip_sub_state = GameTrip(screen_instance)
+        # time_line
+        self.__map_hud = MapHud(
+            screen_instance=screen_instance
+        )
+
+        self.__map_hud.activate()
+
+        # sub_state game trip
+        self.__game_trip_sub_state = GameTrip(screen_instance, self.__map_hud.get_level_jump)
+
 
     def draw(self):
         self.__game_trip_sub_state.draw()
-
+        self.__map_hud.run()
 
     def handle_events(self, events, machine_observer):
 
@@ -29,15 +39,24 @@ class Trip(AbstractState):
 
             if state == 'Again':
                 del self.__game_trip_sub_state
-                self.__game_trip_sub_state = GameTrip(self.__screen)
+
+                if self.__map_hud.is_end():
+                    self._exit = True
+                    machine_observer.ui_class = 'arrival_cinematic'
+                else:
+                    self.__game_trip_sub_state = GameTrip(self.__screen, self.__map_hud.get_level_jump)
+                    self.__map_hud.activate()
 
             if state == 'Jump':
+                self.__map_hud.set_jump()
                 del self.__game_trip_sub_state
                 self.__game_trip_sub_state = JumpTrip(self.__screen)
+                self.__map_hud.deactivate()
 
             if state == 'game_over':
                 del self.__game_trip_sub_state
                 self.__game_trip_sub_state = GameOverTrip(self.__screen)
+                self.__map_hud.deactivate()
 
             if state == 'Home':
                 self._exit = True
