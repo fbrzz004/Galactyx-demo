5599fa40182875ba283bd0c2dfd38b05d2497deb
pcodef <pedro200.sotarios@gmail.com>
2024-10-04 05:43:51 -0500

    Add custom icon for Galactyx Beta v0.1 and set it in the screen initialization
    - Added 'Galactyx Beta v0.1-icon.ico' to the assets/images/icon folder.
    - Set the icon using pygame's display.set_icon() method during screen initialization.

--- code/src/screen.py
+++ code/src/screen.py
@@ -3,6 +3,9 @@ from pygame import (
     display
 )
 
+from pathlib import Path
+from pygame.image import load
+
 class Screen:
     def __init__(self,
                  size: tuple[int, int] | None = None,
@@ -13,7 +16,8 @@ class Screen:
         self.__screen = display.set_mode(size=size) if size else display.set_mode(flags=mode)
 
         self.__title = 'Galactyx'
-        self.__icon_title = None
+        self.__icon_title_path = Path('assets') / 'images' / 'icon' / 'Galactyx Beta v0.1-icon.png'
+        self.__icon_surface = load(str(self.__icon_title_path))
 
         # ---------------- initial methods execute ----------------
         self.__default()
@@ -21,6 +25,7 @@ class Screen:
     # ---------------- privates methods ----------------
     def __default(self):
         display.set_caption(title=self.__title)
+        display.set_icon(self.__icon_surface)
 
     # ---------------- public methods ----------------
     def get_screen(self):
